a4d24fea7c8f199fee86d5067bd5d8aa23a3047b
乾太 ₍₍ ◝(･◡･)◟ ⁾⁾ <kantai.developer@gmail.com>
2025-08-26 03:55:07 +0800

    feat: 將繪師作品列表抽離出來統一管理

---
+++ assets/js/shortcodes/gallery.js
@@ -0,0 +1,41 @@
+function _getDefaultPackeryOptions() {
+  return {
+    percentPosition: true,
+    gutter: 5,
+    resize: true,
+  };
+}
+
+function _getPackeryOptions(nodeGallery) {
+  const defaults = _getDefaultPackeryOptions();
+  const {
+    packeryGutter,
+    packeryPercentPosition,
+    packeryResize,
+  } = nodeGallery.dataset;
+
+  return {
+    percentPosition:
+      packeryPercentPosition !== undefined
+        ? packeryPercentPosition === "true"
+        : defaults.percentPosition,
+    gutter:
+      packeryGutter !== undefined ? parseInt(packeryGutter, 10) : defaults.gutter,
+    resize:
+      packeryResize !== undefined ? packeryResize === "true" : defaults.resize,
+  };
+}
+
+(function init() {
+  $(window).on("load", function () {
+    let packeries = [];
+    // 只選擇 .gallery-artworks，不處理 .gallery-avatar
+    let nodeGalleries = document.querySelectorAll(".gallery-artworks");
+
+    nodeGalleries.forEach((nodeGallery) => {
+      let packery = new Packery(nodeGallery, _getPackeryOptions(nodeGallery));
+      packeries.push(packery);
+    });
+    console.groupEnd();
+  });
+})();
